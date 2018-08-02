139c0c92faf6517cc7eac472c2d336daac37366d
Robert Marsh <nhe6dc@gmail.com>
2018-08-02 09:23:12 -0400

    added store, implemented redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { searchRobots } from "./reducers";
 import "tachyons";
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <div>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </div>,
   document.getElementById("root")
 );
